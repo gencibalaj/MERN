b8bffa5f4b8e7dc9f2847ce8d0519e90c70da199
Genc <gencibalaj@gmail.com>
2022-09-13 12:01:04 +0200

    unit testing with additional assertions

--- app/tests/user.test.js
+++ app/tests/user.test.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose')
 const request = require('supertest')
 const app = require('../app')
 const User = require('../models/User')
-const { hashpassword, genJwt } = require('../helpers/encrypting')
+const { hashpassword } = require('../helpers/encrypting')
 
 const testUserID = new mongoose.Types.ObjectId()
 const testUser = {
@@ -29,7 +29,7 @@ beforeEach(async () => {
     }).save()
 })
 
-
+//USERS testing
 test('Should signup a new user', async () => {
     const response = await request(app).post('/api/users/register').send(testUser).expect(201)
     const user = await User.findById(response.body.user._id)
@@ -71,24 +71,35 @@ test('Should not return profile of the User, without authorization', async () =>
     await request(app).get('/api/profile/me').expect(401)
 })
 
-
+//------------------------------------------------------------------------------------------------------------------
+//TODOS testing
+// return todos
 test('Should return todos of a user', async () => {
-    await request(app).get('/api/todos').set({ Authorization: `Bearer ${token}` }).expect(200)
+    const response = await request(app).get('/api/todos').set({ Authorization: `Bearer ${token}` }).expect(200)
+    expect(response.body.todos).not.toBeNull()
 })
 
-
+// do not return todos
 test('Should not return todos of a user, without authorization', async () => {
     await request(app).get('/api/todos').expect(401)
 })
 
-
+//add a todo
 test('Should add a todo to the user logged in', async () => {
     await request(app).post('/api/todos/add/GYM').set({ Authorization: `Bearer ${token}` }).expect(200)
-    // const todo = await User.findOne
+    const userWithTodo = await User.find({ todo: "GYM" })
+    expect(userWithTodo.length).not.toEqual(0)
 })
 
-
+//remove a todo
 test('Should remove a todo from the user logged in', async () => {
     await request(app).delete('/api/todos/remove/first').set({ Authorization: `Bearer ${token}` }).expect(200)
+    const userWithoutTodo = await User.find({ todo: "first" })
+    expect(userWithoutTodo.length).toEqual(0)
+})
+
+//do not remove a todo
+test('Should not remove a todo from the user because no authorization is provided', async () => {
+    await request(app).delete('/api/todos/remove/first').expect(401)
 })
 
