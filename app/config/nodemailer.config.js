b59081b31b1ea327b230e3685bb692f5f0110880
Genc <gencibalaj@gmail.com>
2022-09-27 10:01:31 +0200

    minor bug fix

--- app/config/nodemailer.config.js
+++ app/config/nodemailer.config.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
-const user = "gencibalaj@gmail.com"
-const pass = "opbszbdvwdoyfekp"
+const user = process.env.user
+const pass = process.env.pass
 
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -18,13 +18,14 @@ const sendEmail = function (name, email, confirmationCode) {
         transport.sendMail({
             from: user,
             to: email,
-            subject: "Please confirm your account",
+            subject: "Please confirm your FSCH account",
             html:
                 `<div>
                 <h1>Email Confirmation</h1>
             <h2>Hello ${name}</h2>
-            <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-            <a href=http://localhost:${process.env.PORT}/api/users/confirm/${confirmationCode}> Click here</a>
+            <p>Thank you for registering to our test app!</p>
+            <p>Please confirm your email by clicking on the following link</p>
+            <a href=http://localhost:${process.env.PORT}/api/users/confirm/${confirmationCode}> Click here to confirm </a>
             </div>`,
         })
 
